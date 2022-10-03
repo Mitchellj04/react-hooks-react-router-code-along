1008e96aba98a43f55a9c1c7dc04cce1cb0f6b60
JustinMitchell <mitchelljm04@gmail.com>
2022-10-03 13:39:11 -0400

    Coded Along Finished

--- src/index.js
+++ src/index.js
@@ -1,7 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter, Route} from "react-router-dom"
+import {BrowserRouter, Route, Switch, NavLink} from "react-router-dom"
 
+
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
+function NavBar(){
+  return(
+    <div>
+      <NavLink 
+      to="/"
+      exact
+      style={linkStyles}
+      activeStyle={{background:"darkblue"}}> 
+      Home
+      </NavLink>
+      <NavLink
+      to="/about"
+      exact
+      style={linkStyles}
+      activeStyle={{background:"darkblue"}}
+      >
+      About 
+      </NavLink>
+      <NavLink
+        to="/login"
+        exact
+        style={linkStyles}
+        activeStyle={{background: "darkblue"}}
+      >
+        Login
+      </NavLink>
+    </div>
+  )
+}
 
 function Home() {
   return (
@@ -38,25 +78,37 @@ function Login() {
 
 function App() {
   return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <NavBar />
     <Switch>
-      <Route path="/about">
+      <Route exact path="/about">
         <About />
       </Route>
-      <Route path="/login">
+      <Route exact path="/login">
         <Login />
       </Route>
       <Route exact path="/">
         <Home />
       </Route>
     </Switch>
-  );
-}
-
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
-
-
